1960b30553c4c1d7339dbcbc31d46c7324e4ba53
NaveenManickDev <naveen.devhub@gmail.com>
2025-06-03 13:31:12 +0530

    Product-Search Functionality Added

--- product.js
+++ product.js
@@ -11,3 +11,30 @@ menuicon.addEventListener("click", function(){
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+
+
+//Product Search Functionality
+
+let productContainer = document.getElementById("product-container")
+
+let search = document.getElementById("search")
+    let productList = productContainer.querySelectorAll("div")
+
+    console.log(productList);
+
+
+    search.addEventListener("keyup", function () {
+        let enteredValue = event.target.value.toUpperCase()
+        // console.log(enteredValue);
+        for (count = 0; count < productList.length; count = count + 1) {
+
+            let productName = productList[count].querySelector("h1").textContent
+
+            if (productName.toUpperCase().indexOf(enteredValue) < 0) {
+                productList[count].style.display = "none"
+            }
+            else {
+                productList[count].style.display = "block"
+            }
+        }
+    })
